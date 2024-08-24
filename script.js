878e615365e34f63ccbde636d0d1956142dff55a
AlinaZah31 <alina_shuvi@yahoo.com>
2024-08-24 22:53:28 +0200

    Update script.js (optimized)

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let time = 0;
-const boardLen = 15, HUNDRED = 100;
+const boardLen = 15, HUNDRED = 100, oneSecond = 1000;
 let linePlane = boardLen - 1, colPlane = linePlane / 2;
 const randomNoOfObjects = Math.floor(Math.random() * boardLen + 1);
 let colObject = [], lineObj = 0;
-for (let i = 1; i <= randomNoOfObjects; ++i) {
-    colObject[i] = Math.floor(Math.random() * boardLen);
-    console.log("obiectele random sunt: " + colObject[i]);
+
+addStrangeObjects();
+
+function addStrangeObjects() {
+    for (let i = 1; i <= randomNoOfObjects; ++i) {
+        colObject[i] = Math.floor(Math.random() * boardLen);
+    }
 }
 
 generateButtons();
@@ -16,9 +20,9 @@ function generateButtons() {
             const button = document.createElement("button");
             button.type = "button";
             button.id = i * HUNDRED + j;
-            button.innerText = button.id;
+            button.innerText = '-';
             document.body.appendChild(button);    
-            if (i == boardLen - 1 && j == 7) {
+            if (i == boardLen - 1 && j == i / 2) {
                 button.style.backgroundColor = "green";
             }
             for (let k = 1; k <= randomNoOfObjects; ++k) {
@@ -33,21 +37,22 @@ function generateButtons() {
     } 
 }
 
+function startStopWatch() {
+    document.getElementById('Score-text').innerHTML = '0';
+    setInterval(increaseTime, oneSecond);
+    setInterval(moveAirplaneUp, oneSecond);
+    setInterval(moveObjectDown, oneSecond);
+}
+
 function increaseTime() {
     for (let i = 1; i <= randomNoOfObjects; ++i) {
-        if (linePlane < 0 || linePlane > 14 || colPlane < 0 || colPlane > 14 || (linePlane == lineObj && colPlane == colObject[i])) {
+        if (linePlane < 0 || linePlane > boardLen - 1 || colPlane < 0 || colPlane > boardLen - 1 || 
+            (linePlane == lineObj && colPlane == colObject[i])) {
             return time;
         }
     }
     ++time;
     document.getElementById('Score-text').innerHTML = time;
-}
-
-function startStopWatch() {
-    document.getElementById('Score-text').innerHTML = '0';
-    setInterval(increaseTime, 1000);
-    setInterval(moveAirplaneUp, 1000);
-    setInterval(moveObjectDown, 1000);
 }
 
 function moveAirplaneUp() {
@@ -87,19 +92,19 @@ function airplaneMovesLeftRight() {
     const log = document.getElementById("log");
     input.addEventListener("keydown", checkKey);
 
-function checkKey(e) {
-  textContent = e.code;
-  let arrowIndicator;
-  if (textContent == "ArrowRight") {
-    arrowIndicator = -1;
-    ++colPlane;
-  } else if (textContent == "ArrowLeft") {
-    arrowIndicator = 1;
-    --colPlane;
-  }
-    const airplane = document.getElementById((linePlane) * HUNDRED + colPlane);
-    airplane.style.backgroundColor = "green";
-    const prevAirplane = document.getElementById((linePlane) * HUNDRED + colPlane + arrowIndicator);
-    prevAirplane.style.backgroundColor = "gray";
-}
+    function checkKey(e) {
+        textContent = e.code;
+        let arrowIndicator;
+        if (textContent == "ArrowRight") {
+            arrowIndicator = -1;
+            ++colPlane;
+        } else if (textContent == "ArrowLeft") {
+            arrowIndicator = 1;
+            --colPlane;
+        }
+        const airplane = document.getElementById((linePlane) * HUNDRED + colPlane);
+        airplane.style.backgroundColor = "green";
+        const prevAirplane = document.getElementById((linePlane) * HUNDRED + colPlane + arrowIndicator);
+        prevAirplane.style.backgroundColor = "gray";
+    }
 }
